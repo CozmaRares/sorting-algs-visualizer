8e655eb27996a80adae96e60f5f8ecaa6eccf154
Cozma Rares <cozmarares11@gmail.com>
2022-10-28 14:09:34 +0300

    Resize canvas on window resize

--- js/sketch.js
+++ js/sketch.js
@@ -5,23 +5,33 @@ let canvasBackground =
     ? 0
     : 255;
 
-function setup() {
+function getCanvasDimensions() {
   const container = document.querySelector("#container");
 
   const styles = getComputedStyle(container);
 
   const parse = str => parseInt(str.replace("px", ""));
 
-  let canvas = createCanvas(parse(styles.width), parse(styles.height));
-  canvas.parent("container");
+  const width = parse(styles.width),
+    height = parse(styles.height);
 
-  strokeWeight(1);
-  noFill();
+  return [width, height];
+}
 
+function setArray() {
   arr = Array.from(
     new Array(width),
     (_, index) => new Element(map(index, 0, width, 0, height), index)
   );
+}
+
+function setup() {
+  let canvas = createCanvas(...getCanvasDimensions());
+  canvas.parent("container");
+
+  strokeWeight(1);
+  noFill();
+  setArray();
 }
 
 function draw() {
@@ -31,4 +41,9 @@ function draw() {
     stroke(el.r, el.g, el.b);
     line(idx, height, idx, height - el.value);
   });
+}
+
+function windowResized() {
+  resizeCanvas(...getCanvasDimensions());
+  setArray();
 }
